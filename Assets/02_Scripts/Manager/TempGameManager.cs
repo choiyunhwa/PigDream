6057be434267a112870113a56b5d5d1a9aaa6e70
choiyunhwa <sjo4208@naver.com>
2024-05-20 11:59:13 +0900

    Fix : Update > Object Prefab, File Name
    오브젝트 파일 위치 변경 및 오브젝트 관련 스크립트 이름 변경
    
    리소스 파일 추가

---
+++ Assets/02_Scripts/Manager/TempGameManager.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempGameManager : MonoBehaviour
+{
+    public static TempGameManager Instance;
+
+    public GameObject Candy;
+    public GameObject Popcorn;
+    public GameObject Hamburger;
+    public GameObject Pizza;
+    public GameObject Dount;
+    public GameObject Honey;
+    public GameObject Cupcake;
+
+    int level = 0;
+    int score = 0;
+
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    void Start()
+    {
+        InvokeRepeating("MakeFood", 0f, 2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void MakeFood()
+    {
+        Instantiate(Candy);
+        Instantiate(Popcorn);
+        Instantiate(Hamburger);
+        Instantiate(Pizza);
+        Instantiate(Dount);
+        Instantiate(Honey);
+        Instantiate(Cupcake);
+
+        if(level == 1)
+        {
+            Instantiate(Popcorn);
+        }
+        else if(level == 2)
+        {
+            Instantiate(Hamburger);
+        }
+        else if(level == 3)
+        {
+            Instantiate(Pizza);
+        }
+        else if (level == 4)
+        {
+            Instantiate(Dount);
+        }
+        else if(level == 5)
+        {
+            Instantiate(Honey);
+        }
+        else if(level == 6)
+        {
+            Instantiate(Cupcake);
+        }
+
+
+    }
+
+    public void GameOver()
+    {
+
+    }
+}
