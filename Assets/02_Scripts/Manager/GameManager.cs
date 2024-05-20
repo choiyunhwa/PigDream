614cf4657b44a1ceca6de04b1c608e25e6c383e7
BaekHyenBeom <167046656+BaekHyenBeom@users.noreply.github.com>
2024-05-20 11:35:05 +0900

    fix : Update -> GameManager,SystemManager position
    캐릭터 배치를 위해 position을 수정했습니다.

--- Assets/02_Scripts/Manager/GameManager.cs
+++ Assets/02_Scripts/Manager/GameManager.cs
@@ -14,6 +14,8 @@ public class GameManager : MonoBehaviour
     [SerializeField] private TextMeshProUGUI currentScoreTxt;
     [SerializeField] private TextMeshProUGUI bestScoreTxt;
 
+
+
     private int currentScore;
 
     void Awake()
